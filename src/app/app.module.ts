6bd77cdd5854e5d3dd793d4adc3813dc68cb8730
wilberquito <wbq.software@gmail.com>
2022-09-28 19:09:05 +0200

    contact area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import {BackgroundComponent} from './background/background.component';
 import { DodecahedronComponent } from './dodecahedron/dodecahedron.component';
 import { BaseComponent } from './base/base.component';
+import {ContactSectionComponent} from './contact-section/contact-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DodecahedronComponent,
     BackgroundComponent,
-    BaseComponent
+    BaseComponent,
+    ContactSectionComponent
   ],
   imports: [
     BrowserModule
