7058a984703326845cbd2be5fd164207586aed53
wilberquito <wbq.software@gmail.com>
2022-09-28 21:30:33 +0200

    fix: angular does not notice video configurations in html

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,13 +1,19 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-background',
   styleUrls: ['./background.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <video class="background__video" src="assets/background/binary_rain.mp4" autoplay muted loop>
+    <video muted="muted" autoplay="true" id="matrix" class="background__video" src="assets/background/binary_rain.mp4" >
     </video>
       `
 })
-export class BackgroundComponent { }
+export class BackgroundComponent implements AfterViewInit { 
+
+    ngAfterViewInit() {
+        const matrix = <HTMLVideoElement> document.getElementById('matrix')
+        matrix.muted = true;
+    }
+}
 
