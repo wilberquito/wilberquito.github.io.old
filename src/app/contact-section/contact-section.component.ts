19c59201a1a87571230ef7150db94acca1d87744
wilberquito <wbq.software@gmail.com>
2022-10-05 23:17:01 +0200

    Sets dodecahedron on top in small screens

--- src/app/contact-section/contact-section.component.ts
+++ src/app/contact-section/contact-section.component.ts
@@ -6,16 +6,17 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
     changeDetection: ChangeDetectionStrategy.OnPush,
     template: `
     <div class="section">
-   <a href="https://github.com/wilberquito/" target="_blank">
-    <i class="fa-brands fa-github" ></i>
-    </a>
-   <a href="https://linkedin.com/in/wilber-quito/" target="_blank">
-    <i class="fa-brands fa-linkedin-in"></i>
-    </a>
-   <a href="https://instagram.com/wilberquito" target="_blank">
-    <i class="fa-brands fa-instagram"></i>
-    </a>
- 
+        <a href="https://github.com/wilberquito/" target="_blank">
+            <i class="fa-brands fa-github" ></i>
+        </a>
+        <a href="https://linkedin.com/in/wilber-quito/" target="_blank">
+            <i class="fa-brands fa-linkedin-in"></i>
+        </a>
+        <a href="https://instagram.com/wilberquito" target="_blank">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+    </div>
+
     `
 })
 export class ContactSectionComponent { }
