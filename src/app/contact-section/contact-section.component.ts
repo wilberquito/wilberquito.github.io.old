bf80e236b195276556b7d14d9f96e312ef1bc52f
wilberquito <wbq.software@gmail.com>
2022-10-22 18:30:05 +0200

    solves typo html error

--- src/app/contact-section/contact-section.component.ts
+++ src/app/contact-section/contact-section.component.ts
@@ -10,10 +10,10 @@ import { OverContactService } from '../service/over-contact.service'
         <a (mouseenter)=enter() (mouseleave)=leave() href="https://github.com/wilberquito/" target="_blank">
             <i class="fa-brands fa-github" ></i>
         </a>
-        <a (mouseenter)=enter() (mouseleave)=leave() hhref="https://linkedin.com/in/wilber-quito/" target="_blank">
+        <a (mouseenter)=enter() (mouseleave)=leave() href="https://linkedin.com/in/wilber-quito/" target="_blank">
             <i class="fa-brands fa-linkedin-in"></i>
         </a>
-        <a (mouseenter)=enter() (mouseleave)=leave() hhref="https://instagram.com/wilberquito" target="_blank">
+        <a (mouseenter)=enter() (mouseleave)=leave() href="https://instagram.com/wilberquito" target="_blank">
             <i class="fa-brands fa-instagram"></i>
         </a>
     </div>
